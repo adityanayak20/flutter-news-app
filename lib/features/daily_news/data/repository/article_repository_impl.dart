67f0855f9d6d5723d0978e69b508588553152fc0
Aditya Nayak <20anayakl@google.com>
2023-07-24 17:01:54 +0530

    Repository Classses & Models Created

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,12 +1,30 @@
+import '../models/article.dart';
 import '../../../../core/resources/data_state.dart';
 import '../../domain/entities/article.dart';
 import '../../domain/repository/article_repository.dart';
-
 
 class ArticleRepositoryImpl implements ArticleRepository {
   @override
   Future<DataState<List<ArticleModel>>> getNewsArticles() async {
     // TODO: implement getNewsArticles
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ArticleEntity>> getSavedArticles() {
+    // TODO: implement getSavedArticles
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeArticle(ArticleEntity article) {
+    // TODO: implement removeArticle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> saveArticle(ArticleEntity article) {
+    // TODO: implement saveArticle
     throw UnimplementedError();
   }
 }
