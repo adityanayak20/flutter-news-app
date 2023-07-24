f81b599e9efa51678c7bdc9eb22f50bbe3ebe28d
Aditya Nayak <20anayakl@google.com>
2023-07-24 16:04:36 +0530

    Packages added & Folder Structure created

---
+++ android/app/src/main/kotlin/com/aditya/flutter_news_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aditya.flutter_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(primarySwatch: Colors.green),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Container(),
+    );
+  }
+}
